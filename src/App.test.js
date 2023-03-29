efc42c9ab5e71eaddaa8aac5405c9369c6d43d8c
Tyler Randall <tyler7r@gmail.com>
2023-03-29 13:12:13 -0400

    Set up dev env

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import RouteSwitch from './RouteSwitch';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<RouteSwitch />);
+  screen.getByRole('heading', {name: 'Hello World'});
 });
