1dffafa9aabd2834d23e4e8737922433ea679f1f
Tyler Randall <tyler7r@gmail.com>
2023-04-05 13:36:29 -0400

    Updates

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-const Home = (props) => {
-    return (
-        <div className='content'>
-            <img id='homeLogo' alt='homeLogo' src={require('../images/logo.png')} />
-        </div>
-    )
-}
-
-export default Home;
